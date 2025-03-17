afc0e3b286c00810350a9f2e1498755b969cd637
DominikHommer <dominik.hommer@web.de>
2025-03-17 16:46:52 +0100

    Initial Commit

---
+++ scAIper/scAIperApp.swift
@@ -0,0 +1,17 @@
+//
+//  scAIperApp.swift
+//  scAIper
+//
+//  Created by Dominik Hommer on 17.03.25.
+//
+
+import SwiftUI
+
+@main
+struct scAIperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scAIperTests/scAIperTests.swift
@@ -0,0 +1,17 @@
+//
+//  scAIperTests.swift
+//  scAIperTests
+//
+//  Created by Dominik Hommer on 17.03.25.
+//
+
+import Testing
+@testable import scAIper
+
+struct scAIperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
